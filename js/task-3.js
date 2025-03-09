dd2dd43d1b454f5265360371cf4f540f3eea38f2
Marina <marina.slepinina3@gmail.com>
2025-03-09 21:27:38 +0200

    corrections to task 3

--- js/task-3.js
+++ js/task-3.js
@@ -7,5 +7,5 @@ function handleInput(event) {
     const userInput = event.target.value.trim();
     if (userInput !== "") {
         output.textContent = userInput;
-    }
+    } else output.textContent = "Anonymous";
 }
